f459cb588ed7d693fc886e9dab4f90620de35df7
knthiem <44018540+knthiem@users.noreply.github.com>
2018-10-30 17:36:59 -0400

    Update Qualifications.java
    Added comments

--- LoginForm/src/Qualifications.java
+++ LoginForm/src/Qualifications.java
@@ -14,7 +14,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class Qualifications extends javax.swing.JFrame {
-    DefaultTableModel model;
+    //Variable used to connect to the database and retrieve data
     Connection conn;
     ResultSet rs;
 
@@ -206,33 +206,40 @@ public class Qualifications extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-   
+    
+    //When the user clicks the exit button, the program ends
     private void bExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bExitActionPerformed
         System.exit(0);
     }//GEN-LAST:event_bExitActionPerformed
 
+    //The add method when user clicks the add button
     private void bAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bAddActionPerformed
         createConnection();
+        //Creating text field for a panel
         JTextField qualIDField = new JTextField("");
         JTextField descriptionField = new JTextField("");
-        
+        //Variables that will be used to store user input
         String qualID, description;
         
+        //Creating a panel
         JPanel panel = new JPanel(new GridLayout(0, 1));
         panel.add(new JLabel("QualificationID:"));
         panel.add(qualIDField);
         panel.add(new JLabel("Description:"));
         panel.add(descriptionField);
-
+        
+        
         int result = JOptionPane.showConfirmDialog(null, panel, "Add Qualification Form",
             JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        //When the user clicks the ok option
         if (result == JOptionPane.OK_OPTION) {
             qualID = qualIDField.getText();
             description = descriptionField.getText();
 
-            
+            //SQL statement to insert value
             String sql = "INSERT INTO qualification VALUES ('" + qualID + "', '"
                     + description +"')";
+            //Connecting to the database and perform the statement
             try{
                 PreparedStatement ps = conn.prepareStatement(sql);
                 ps.executeQuery();
@@ -243,38 +250,50 @@ public class Qualifications extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, "invalid data");
                 System.out.println(e);
             }
+        //If the user click cancels, nothing happens to the database
         } else {
             System.out.println("Cancelled");
         }
         
     }//GEN-LAST:event_bAddActionPerformed
 
+    //Method for the logout button
+    //When the user presses the logout button, the user will be returned to the login form
     private void bLogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bLogoutActionPerformed
         this.setVisible(false);
         Login l1 = new Login();
         l1.setVisible(true);        
     }//GEN-LAST:event_bLogoutActionPerformed
 
+    //Method for the back button
+    //When the user clicks the back button, the main menu will be shown
     private void bBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bBackActionPerformed
         this.setVisible(false);
         mainMenu m1 = new mainMenu();
         m1.setVisible(true);
     }//GEN-LAST:event_bBackActionPerformed
 
+    //Method for the search button
+    //The user can search for qualification in the search bar
     private void bSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bSearchActionPerformed
         createConnection();
+        //SQL statement using the user input
         String sql = "select * from qualification where lower(QualDesc) = "
                 + "'" + tSearchBar.getText().toLowerCase() + "'";
         getResultSet(sql, "no qualification found!");
         closeConnection();
     }//GEN-LAST:event_bSearchActionPerformed
 
+    //Method when this form loads
+    //Updates the data shown in the table by connecting to the database
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         createConnection();
         updateTable();
         closeConnection();
     }//GEN-LAST:event_formWindowOpened
 
+    //Method for when the user types something in the search bar
+    //The table should be constantly updated each user enters a character
     private void tSearchBarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tSearchBarActionPerformed
         tSearchBar.addKeyListener(new KeyListener() { 
             public void keyTyped(KeyEvent event) {
@@ -301,11 +320,17 @@ public class Qualifications extends javax.swing.JFrame {
         });
     }//GEN-LAST:event_tSearchBarActionPerformed
 
+    //Method for the delete button
+    //When user presses the button, the selected row will be deleted
     private void bDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bDeleteActionPerformed
         createConnection();
+        //Checking which row is selected
         int rowSelected = tbQualifications.getSelectedRow();
+        //Get the Qualification ID from the row
         String selectQualID = tbQualifications.getValueAt(rowSelected, 0).toString();
+        //SQL statement to delete a row by the qualification id
         String sql = "delete from qualification where QualID = '" + selectQualID + "'";
+        //Connect to the database and perform SQL statement
         try{
             PreparedStatement ps = conn.prepareStatement(sql);
             rs = ps.executeQuery();
@@ -318,21 +343,28 @@ public class Qualifications extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_bDeleteActionPerformed
 
+    //Method for the modify button
+    //When user clicks on the modify button, the selected row will be modified
+    //The logic to modify the table is by first deleting the row selected, then readding the row with new values
     private void bModifyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bModifyActionPerformed
+        //Checks which row is selected
         if(tbQualifications.getSelectedRow() == -1)
             JOptionPane.showMessageDialog(null, "Please select a row to modify!");
         else{
             createConnection();
+            //Get the information needed from the rows
             int rowSelected = tbQualifications.getSelectedRow();
             String selectQualID = tbQualifications.getValueAt(rowSelected, 0).toString();
             String selectDescription = tbQualifications.getValueAt(rowSelected, 1).toString();
             
-            
+            //Create text fields for a panel
             JTextField qualIDField = new JTextField(selectQualID);
             JTextField descriptionField = new JTextField(selectDescription);
             
+            //Variables used to store user input
             String qualid, description;
 
+            //Creating a panel
             JPanel panel = new JPanel(new GridLayout(0, 1));
             panel.add(new JLabel("QualificationID:"));
             panel.add(qualIDField);
@@ -342,15 +374,17 @@ public class Qualifications extends javax.swing.JFrame {
             int result = JOptionPane.showConfirmDialog(null, panel, "Modify Qualification Form",
                 JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
             //UIManager.put("OptionPane.okButtonText", "Save");
+            //When user clicks ok, the inputs will be stored
             if (result == JOptionPane.OK_OPTION) {
                 qualid = qualIDField.getText();
                 description = descriptionField.getText();
 
-
+                //SQL statement to delete and insert
                 String sql1 = "delete from qualification where QualID = '" + 
                         selectQualID + "'";
                 String sql2 = "INSERT INTO qualification VALUES ('" + qualid + "', '"+ 
                         description + "')";
+                //Connecting to the database
                 try{
                     PreparedStatement ps = conn.prepareStatement(sql1);
                     PreparedStatement ps2 = conn.prepareStatement(sql2);
@@ -377,15 +411,19 @@ public class Qualifications extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_bModifyActionPerformed
 
+    //Empties the text in the search bar
     private void tSearchBarFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_tSearchBarFocusGained
         if(tSearchBar.getText().equals("Search qualification..."))
             tSearchBar.setText("");
     }//GEN-LAST:event_tSearchBarFocusGained
 
+    //Resets the text in the search bar to the default value
     private void tSearchBarFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_tSearchBarFocusLost
         if(tSearchBar.getText().equals(""))
             tSearchBar.setText("Search qualification...");
     }//GEN-LAST:event_tSearchBarFocusLost
+    
+    //Method to creae a connection to the oracle database
     public void createConnection(){
         try{
             conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "ZEINAJK", "Welcome1");
@@ -395,6 +433,8 @@ public class Qualifications extends javax.swing.JFrame {
             System.out.println(e);
         }
     }
+    
+    //Method to close connection
     public void closeConnection(){
         try{
             conn.close();
@@ -404,6 +444,8 @@ public class Qualifications extends javax.swing.JFrame {
             System.out.println(e);
         }
     }
+    
+    //Method to get results from a SQL statement
     public ResultSet getResultSet(String sql, String errorMsg){
         try{
             PreparedStatement ps = conn.prepareStatement(sql);
@@ -417,29 +459,13 @@ public class Qualifications extends javax.swing.JFrame {
         }
         return rs;
     }
+    
+    //Method to update the data shown in the Jtable
     public void updateTable(){
         String sql = "select * from qualification";
         getResultSet(sql, "no qualification found!");
     }
     
-//    public static void applyModifications(String[] newInfo){
-//        String sql1 = "delete from qualification where HCN = '" + selectHCN + "'";
-//        String sql2 = "INSERT INTO qualification VALUES ('" + newInfo[0] + "', '"+ newInfo[1] +
-//                "', '" + newInfo[2] + "', TO_DATE('" + newInfo[3] + "','yyyy/mm/dd'), " +
-//                newInfo[4] + "," + "'" + newInfo[5] + "')";
-//        try{
-//            PreparedStatement ps = conn.prepareStatement(sql1);
-//            ps.executeQuery();
-//            PreparedStatement ps2 = conn.prepareStatement(sql2);
-//            ps2.executeQuery();
-//            updateTable();
-//            closeConnection();
-//        }
-//        catch(Exception e){
-//            JOptionPane.showMessageDialog(null, "invalid data");
-//            System.out.println(e);
-//        }
-//    }
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
